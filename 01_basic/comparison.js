4e090ecd9ba646270bc8b8e66e8f303ba2830097
Rohit Senapati <141651611+rohit08sen@users.noreply.github.com>
2023-10-22 17:24:18 +0000

    Comparison

---
+++ 01_basic/comparison.js
@@ -0,0 +1,8 @@
+console.log("2">1);//true
+console.log("02">1);//true
+
+console.log(null>0);//false
+console.log(null==0);//false null converted to 0
+console.log(null>=0);//true
+
+// === it check value with datatype

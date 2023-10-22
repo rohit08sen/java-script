3169277ba509d8d3c3a98f692dd0257b469445c2
Rohit Senapati <141651611+rohit08sen@users.noreply.github.com>
2023-10-22 13:35:37 +0000

    varibles in java

---
+++ 01_basic/variables.js
@@ -0,0 +1,14 @@
+const accountId=14345;
+let accountEmail="omm54@gmail.com"
+var accountPassword="78652"
+accountCity="Baripada"  /*it is wrong but work */
+
+//accountId=7885 not allowed
+
+
+console.log(accountId);
+
+console.table([accountEmail,accountPassword,accountCity])
+
+/*prefer not to used var
+bcoz of issue in block scope functional scope*/

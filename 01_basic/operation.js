a2ba726f7b68e11a5c2de4a254fe00a984dd1ebb
Rohit Senapati <141651611+rohit08sen@users.noreply.github.com>
2023-10-22 17:14:37 +0000

    operations

---
+++ 01_basic/operation.js
@@ -0,0 +1,17 @@
+let value=3;
+let negValue= -value;
+console.log(negValue);
+
+// basic arithmetic operation
+
+let str1="Hello";
+let str2=" Rohit"
+let str3=str1+str2;
+console.log(str3);
+
+console.log("1"+2);//12
+console.log(1+"2");//12
+console.log("1"+2+2);//122
+console.log("1"+(2+2));//14
+console.log(2+3+"1");//51
+

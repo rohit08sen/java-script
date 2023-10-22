bab503f67964b0fd71c9464be9a20b708ca68ebb
Rohit Senapati <141651611+rohit08sen@users.noreply.github.com>
2023-10-22 13:59:17 +0000

    DataTypes

---
+++ 01_basic/datatype.js
@@ -0,0 +1,17 @@
+"use strict"; //treat all JS code newer version
+
+// alert(3+3); we are using nodejs,not browser
+
+// number=> 2^52
+// bigint
+// string =>""
+// boolean => true/false
+// null => standalone value  empty
+// undefined=> 
+// symbol => unique
+
+
+// object
+
+console.log(typeof "Rohit");
+console.log(typeof null); //object

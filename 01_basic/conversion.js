3e5edd4e6a17521d11d1026c75942d78954de17f
Rohit Senapati <141651611+rohit08sen@users.noreply.github.com>
2023-10-22 15:01:16 +0000

    Conversion of Differnet DataTypes

---
+++ 01_basic/conversion.js
@@ -0,0 +1,23 @@
+let score="32abc";
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber= Number(score); //number conversion
+console.log(typeof valueInNumber);
+
+console.log(valueInNumber);//NaN
+
+// "34"=>34
+// "34abc"=>NaN
+// true=>1
+
+let isLoggedIn=1;
+
+let booleanIsLoggedIn=Boolean(isLoggedIn); //boolean conversion
+console.log(booleanIsLoggedIn);
+
+// 1=>true;
+// ""=>false;
+// "rohit"=>true
+

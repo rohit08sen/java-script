0f848a944be34a6951300d06bd9884c5557cd280
Rohit Senapati <141651611+rohit08sen@users.noreply.github.com>
2023-10-24 18:20:59 +0000

    Function

---
+++ 03_Basic/function.js
@@ -0,0 +1,15 @@
+//Declaration
+function functionName(){
+    console.log("erty");
+    console.log("ertgch");
+    console.log(",mnbv");
+}
+
+functionName;//reference
+functionName();//function call
+
+function add(n1,n2){//here n1 n2 parameters
+    console.log(n1+n2);
+}
+
+add(7,9);//here 7,9 are arguments

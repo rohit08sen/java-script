4985e2beb0df82f3258537cadbea777b7a6a45f9
Rohit Senapati <141651611+rohit08sen@users.noreply.github.com>
2023-10-24 17:56:08 +0000

    Some method  related to object

--- 02_Basic/object_02.js
+++ 02_Basic/object_02.js
@@ -33,3 +33,27 @@ const obj2={
 
 const obj3={...obj1,...obj2};
 console.log(obj3);
+
+const users=[
+    {
+        id:1,
+        email:"xvbng@345.com"
+    },
+    {
+        id:5,
+        email:"xvbng@3565.com"
+    },
+    {
+        id:6,
+        email:"xvbng@3456.com"
+    }
+]
+
+users[1].email;
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));//[ 'id', 'name' ] array
+console.log(Object.values(tinderUser));//[ '123rfg', 'rohit' ]
+console.log(Object.entries(tinderUser));//[ [ 'id', '123rfg' ], [ 'name', 'rohit' ] ]
+
+console.log(tinderUser.hasOwnProperty("name"));//To check whether property is present or not.

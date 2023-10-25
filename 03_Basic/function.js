7173eda87e69037f646bad6b7039de14e8895009
Rohit Senapati <141651611+rohit08sen@users.noreply.github.com>
2023-10-25 04:15:07 +0000

    FUNCTION MODIFIED

--- 03_Basic/function.js
+++ 03_Basic/function.js
@@ -11,5 +11,36 @@ functionName();//function call
 function add(n1,n2){//here n1 n2 parameters
     console.log(n1+n2);
 }
+function add(n1,n2){//here n1 n2 parameters
+    // console.log(n1+n2);
+    let result=n1+n2;
+    return result;
+}
 
 add(7,9);//here 7,9 are arguments
+const result=add(5,8);
+console.log(result);//undefined why??
+
+function userlogin(username){
+    return `${username} just logged in`
+}
+console.log(userlogin("Rohit"));
+
+
+//shoping cart
+function calculateCartPrice(...num1){//rest operator for take as much as parameter
+    return num1;
+}
+console.log(calculateCartPrice(200,345,678));
+
+//how to pass object in function
+const user={
+    username:"Rohit",
+    id:"q67"
+}
+
+function handleObject(anyObject){
+    console.log(`Username is ${anyObject.username} and id is ${anyObject.id}`);
+}
+
+handleObject(user);

e3c8796b70f443ef03ed02b6ecbe06a613e95b4d
Rohit Senapati <141651611+rohit08sen@users.noreply.github.com>
2023-10-26 14:44:46 +0000

    Modified

--- 03_Basic/scope.js
+++ 03_Basic/scope.js
@@ -1,3 +1,18 @@
-let a=10;
-const b=20;
-var c=30;
+// let a=10;
+// const b=20;
+// var c=30;
+
+// console.log(a,b,c);//output is coming but why different data type is used?
+//solution
+let a=300;
+
+if (true){
+    let a=10;
+    const b=20;
+    // var c=30;
+    console.log("Inner ",a);//scope
+}
+
+console.log(a);//console.log(a);^ error not in scope ....300 global scope
+console.log(b);
+console.log(c);

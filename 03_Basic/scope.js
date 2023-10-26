5d90fcddf0ea16d780c6246730701505eba570fc
Rohit Senapati <141651611+rohit08sen@users.noreply.github.com>
2023-10-26 14:59:39 +0000

    Scope in Js

--- 03_Basic/scope.js
+++ 03_Basic/scope.js
@@ -13,6 +13,38 @@ if (true){
     console.log("Inner ",a);//scope
 }
 
-console.log(a);//console.log(a);^ error not in scope ....300 global scope
-console.log(b);
-console.log(c);
+
+
+
+// console.log(a);//console.log(a);^ error not in scope ....300 global scope
+// console.log(b);
+// console.log(c);
+
+//Nested scope
+
+function one(){
+    const username="Rohit";
+
+    function two(){
+        const website="Youtube";
+        console.log(username);//won't give error ....child can excess from parent but not vice-versa.
+    }
+    // console.log(website);//give error out of scope
+
+    two();
+}
+
+one();
+
+//**********************interesting******************** 
+console.log(addone(5));//won't give error
+function addone(value){
+    return value+1;
+}
+// addone(5);
+
+console.log(addTwo(5));//it will give error variable can't found
+const addTwo=function(value){//this called expression
+    return value+2;
+}
+// addTwo(5);
